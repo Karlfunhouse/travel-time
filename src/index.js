1b07dabb1705b702698b2553a0d9f32481ae0dc8
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-27 20:21:06 -0700

    :crystal_ball: changed login logic to fetch userData when agency logs in

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import domUpdates from './domUpdates.js'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/travel-icon.png'
 
-let isTraveler = (name) => {
+export const isTraveler = (name) => {
   if(!name.includes('traveler')) {
     return false;
   }
@@ -21,30 +22,65 @@ let isTraveler = (name) => {
     return true
 }
 
-let checkLoginCredentials = (event) => {
+export const checkLoginCredentials = (event) => {
   event.preventDefault()
   let userName = event.target[0].value
   let password = event.target[1].value
-  if(userName === 'agency') {
+  let id = parseInt(userName.slice(8))
+  if (userName === 'agency' && password === 'travel2020') { //valid agent
     console.log('yer an agent');
-  } else if(isTraveler(userName)) {
+    domUpdates.showUsers();
+    //load agent dashboard
+  } else if (userName === 'agency' && password !== 'travel2020') {//invalid
+    window.alert('☠️Invalid Password☠️')
+    return 'invalid password'
+  } else if (isTraveler(userName) && password === 'travel2020') {//valid
     console.log('verified user');
+    
+  } else if (isTraveler(userName) && password !== 'travel2020'){//invalid
+    window.alert('☠️Invalid Password☠️')
+    return 'invalid password'
+    //fetch all trips that match that user's id
+
+    //fetch all destinations that currently exist
+    //instantiate new Traveler
+    //load traveler dashboard
   } else {
-    alert('☠️Invalid Username☠️')
-    return
+    window.alert('☠️Invalid Username☠️')
+    return 'invalid user'
   }
   if(password !== 'travel2020') {
-    alert('☠️Invalid Password☠️')
-    return
+    window.alert('☠️Invalid Password☠️')
+    return 'invalid password'
   } else {
-    console.log('Lets GO!');
+    console.log('Login Successful');
   }
-
-//if userName !== an appropriate username or agency give error message
-//else
-// check password = travel2020
-//if password !== travel2020 give error message
-// else take user to appropriate page
 }
 
+// const userData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+//   .then(response => response.json())
+//   .then(data => data.travelers)
+//   .catch(error => console.log('travelers error'))
+//
+// const tripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
+//   .then(response => response.json())
+//   .then(data => data.trips)
+//   .catch(error => console.log('trips error'))
+//
+// const destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+//   .then(response => response.json())
+//   .then(data => data.destinations)
+//   .catch(error => console.log('destinations error'))
+//
+// Promise.all([userData, tripsData, ])
+
+
+
 $('#login-form').submit(checkLoginCredentials)
+// $('.login-button').click(helper)
+//
+// function helper() {
+//   event.preventDefault();
+//   console.log('click');
+//   domUpdates.showUsers();
+// }
