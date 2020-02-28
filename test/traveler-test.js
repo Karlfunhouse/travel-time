89c3cb1898aef428a19f2c5e132220a3dcb2b321
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-27 20:23:06 -0700

    :bar_chart: added first tests for traveler class

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -7,9 +7,19 @@ let traveler;
 describe("traveler", function() {
   beforeEach("Function", function() {
     traveler = new Traveler(travelerData.travelers[0]);
-  })
+  });
+
+  it("should be a function", function() {
+    expect(Traveler).to.be.a('function')
+  });
 
   it("should be an instance of Traveler", function() {
     expect(traveler).to.be.an.instanceof(Traveler)
   });
+
+  it("should be able to get travelers name", function() {
+    expect(traveler.name).to.equal('Ham Leadbeater')
+  })
+
+  
 })
