82741bb88843d1555e9240b6b177ddac204bffec
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-03-02 13:36:53 -0700

    :crystal_ball: Added method to getTripsById for a user; working on calculate Total Spent

--- src/traveler.js
+++ src/traveler.js
@@ -1,14 +1,32 @@
+import dataParser from './dataParser'
+
 class Traveler {
   constructor(travelerData) {
+    console.log(travelerData);
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
     this.username = "traveler" + this.id;
     this.password = "travel2020";
+    this.trips = this.getTripsById() || []
   }
 
+//
 
+getTripsById() {
+  return dataParser.fetchTripsData()
+    .then(allTrips => {
+      return allTrips.trips.filter(trip => trip.userID === this.id)
+    })
+}
 
+calculateTotalSpent() {
+  console.log(getTripsById());
+}
+
+bookTrip() {
+
+}
 }
 
 export default Traveler;
